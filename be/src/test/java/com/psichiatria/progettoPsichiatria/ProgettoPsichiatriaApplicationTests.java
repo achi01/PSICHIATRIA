89b3e54b2538867648723c4244dbe7cd12637d66
Achi01 <amarachi.ezeifedi@studenti.unitn.it>
2025-01-26 16:38:16 +0100

    Aggiunta delle basi del frontend e backend.

---
+++ be/src/test/java/com/psichiatria/progettoPsichiatria/ProgettoPsichiatriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psichiatria.progettoPsichiatria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoPsichiatriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
